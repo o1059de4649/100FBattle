a86ab7017032d2d384aca2b9d5a908ecbab51284
o1059de4649 <agi.13ms@gmail.com>
2018-11-21 08:42:57 +0900

    furnitureUpdate

--- Assets/Script/MyRoomSaveSystem.cs
+++ Assets/Script/MyRoomSaveSystem.cs
@@ -22,6 +22,26 @@ namespace UnityStandardAssets.CrossPlatformInput
                 this.gameObject.transform.position = new Vector3 (float.Parse(_splitType[0]),float.Parse(_splitType[1]),float.Parse(_splitType[2]));
                 this.gameObject.transform.localEulerAngles = new Vector3(float.Parse(_splitType[3]), float.Parse(_splitType[4]), float.Parse(_splitType[5]));
             }
+
+            if (this.gameObject.name == "Table" && PlayerPrefs.HasKey("Table") == true)
+            {
+                //座標挿入
+                dataString[0] = PlayerPrefs.GetString(this.gameObject.name, "default");
+                _splitType = dataString[0].Split(new[] { ',', '(', ')' }, System.StringSplitOptions.RemoveEmptyEntries);
+
+                this.gameObject.transform.position = new Vector3(float.Parse(_splitType[0]), float.Parse(_splitType[1]), float.Parse(_splitType[2]));
+                this.gameObject.transform.localEulerAngles = new Vector3(float.Parse(_splitType[3]), float.Parse(_splitType[4]), float.Parse(_splitType[5]));
+            }
+
+            if (this.gameObject.name == "Chair" && PlayerPrefs.HasKey("Chair") == true)
+            {
+                //座標挿入
+                dataString[0] = PlayerPrefs.GetString(this.gameObject.name, "default");
+                _splitType = dataString[0].Split(new[] { ',', '(', ')' }, System.StringSplitOptions.RemoveEmptyEntries);
+
+                this.gameObject.transform.position = new Vector3(float.Parse(_splitType[0]), float.Parse(_splitType[1]), float.Parse(_splitType[2]));
+                this.gameObject.transform.localEulerAngles = new Vector3(float.Parse(_splitType[3]), float.Parse(_splitType[4]), float.Parse(_splitType[5]));
+            }
         }
 
       
@@ -34,7 +54,7 @@ namespace UnityStandardAssets.CrossPlatformInput
 
         public void SaveMyRoom()//位置保存
         {
-            //Cube(試作)
+            //Mirror(試作)
             if (this.gameObject.name == "Mirror")
             {
                 _obj[0] = this.gameObject;
@@ -43,8 +63,31 @@ namespace UnityStandardAssets.CrossPlatformInput
                 PlayerPrefs.SetString(this.gameObject.name, dataString[0]);
                 PlayerPrefs.Save();
             }
+
+            //Table(試作)
+            if (this.gameObject.name == "Table")
+            {
+                _obj[0] = this.gameObject;
+                dataString[0] = _obj[0].transform.position.ToString() + "," + _obj[0].transform.localEulerAngles.ToString();
+
+                PlayerPrefs.SetString(this.gameObject.name, dataString[0]);
+                PlayerPrefs.Save();
+            }
+
+            //Chair(試作)
+            if (this.gameObject.name == "Chair")
+            {
+                _obj[0] = this.gameObject;
+                dataString[0] = _obj[0].transform.position.ToString() + "," + _obj[0].transform.localEulerAngles.ToString();
+
+                PlayerPrefs.SetString(this.gameObject.name, dataString[0]);
+                PlayerPrefs.Save();
+            }
         }
 
+
+
+        //Move
         public void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.tag == "Player")
